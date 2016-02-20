73c35ecca7ab1b1ac741a3e3c82bc2470e7a6653
Marcus <mdaygmdesigner@gmail.com>
2016-02-20 16:36:53 +1100

    Friend prefab
    Friend move & friend game master script
    
    Friend mechanic test scene

---
+++ Poetry Platformer/Assets/Scripts/Friend/FriendGameMaster.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendGameMaster : MonoBehaviour {
+
+
+    public NewPlayerMove playerMove;
+    public FriendMove friendMove;
+
+    public bool playerSwitched;
+
+	// Use this for initialization
+	void Start ()
+    {
+        friendMove.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            playerSwitched = !playerSwitched;
+          
+        }
+
+        if (playerSwitched)
+        {
+            playerMove.enabled = false;
+            friendMove.enabled = true;
+        }
+        else if (!playerSwitched)
+        {
+            playerMove.enabled = true;
+            friendMove.enabled = false;
+
+        }
+
+
+	
+	}
+}
